691c8d2501952c37883245044229c85c969a01ee
hyojung64 <87048623+hyojung64@users.noreply.github.com>
2021-08-31 14:35:18 +0900

    Add files via upload

---
+++ src/main/java/CafeOrder/PaymentRepository.java
@@ -0,0 +1,10 @@
+package CafeOrder;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface PaymentRepository extends PagingAndSortingRepository<Payment, Long>{
+
+	List<Payment> findByOrderId(Long orderId);
+}
